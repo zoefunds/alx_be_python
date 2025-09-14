d0f956466e1aed502303e700e7de70d5e0a77409
Precious <preciousmofeoluwa@gmail.com>
2025-09-14 21:24:00 +0100

    update

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
